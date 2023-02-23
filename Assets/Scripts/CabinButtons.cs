5cc87798f19d505f0e992040425accae09fed34c
NakNamPotchiii <62009152+NakNamPotchi@users.noreply.github.com>
2023-02-23 09:17:49 +0800

    Major change
    Finished Introduction Toresmundo
    Halfway with Cabin features

---
+++ Assets/Scripts/CabinButtons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CabinButtons : MonoBehaviour
+{
+    public void ClickPortal()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void ClickAlmanac()
+    {
+        
+    }
+}
