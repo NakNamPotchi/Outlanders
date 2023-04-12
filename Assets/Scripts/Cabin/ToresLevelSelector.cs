eb7e69a07e3a5b9a885fbae3ef895ea626923b30
NakNamPotchiii <62009152+NakNamPotchi@users.noreply.github.com>
2023-04-12 23:55:45 +0800

    Revisions (1.6.3)
    - In Encyclopedia, use characters as icons instead of numbers
    - Stage 3? bug with Hearts
    - Stage 10 but with Mooltos 6 not unlocking
    - Tutorial color, remove color or keep consistent with other buttons

--- Assets/Scripts/Cabin/ToresLevelSelector.cs
+++ Assets/Scripts/Cabin/ToresLevelSelector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,9 @@ public class ToresLevelSelector : MonoBehaviour
     public SceneFader fader;
 
     public Button[] levelButtons;
+    public Image[] levelImages;
+    public TextMeshProUGUI[] levelTexts;
+    [SerializeField] private Sprite[] unlockedToresIcons;
 
     private int MooltosBookSelector;
 
@@ -19,7 +23,15 @@ public class ToresLevelSelector : MonoBehaviour
         for (int i = 0; i < levelButtons.Length; i++)
         {
             if (i + 1 > MooltosBookSelector)
+            {
                 levelButtons[i].interactable = false;
+                levelTexts[i].text = "?";
+            }
+            else
+            {
+                levelButtons[i].interactable = true;
+                levelImages[i].sprite = unlockedToresIcons[i];
+            }
         }
     }
 
