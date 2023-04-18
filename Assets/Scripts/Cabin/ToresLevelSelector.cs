279f917612cc0ba92a1a39b0a77d7e5d8f599328
NakNamPotchiii <62009152+NakNamPotchi@users.noreply.github.com>
2023-04-19 02:09:45 +0800

    Revisions 1.6.4
    - Remove "Welcome to Toresmundo" in Tutorial
    - Highlight path when introducing spawn arrows
    - Make "Duwende" spawn after placing tower in tutorial
    - Slower "Duwende" for tutorial
    - Stats for Mooltos
    - music slider change responds instantly and no need to click back to apply change
    - Locked Tower? Still clickable buttons but add notification which stage to unlock it. This is for encyclopedia
    - Add "about" (?) beside Quit button in Main Menu and provide credits to creators of assets.

--- Assets/Scripts/Cabin/ToresLevelSelector.cs
+++ Assets/Scripts/Cabin/ToresLevelSelector.cs
@@ -13,18 +13,21 @@ public class ToresLevelSelector : MonoBehaviour
     public Image[] levelImages;
     public TextMeshProUGUI[] levelTexts;
     [SerializeField] private Sprite[] unlockedToresIcons;
+    [SerializeField] private GameObject[] unlockWhen;
+    [SerializeField] private GameObject toresSection;
+    [SerializeField] private GameObject unlockedToresSection;
+    [SerializeField] private GameObject[] unlockedTores;
 
-    private int MooltosBookSelector;
+    private int ToresBookSelector;
 
     void Start()
     {
-        int MooltosBookSelector = PlayerPrefs.GetInt("MooltosBookSelector");
+        int ToresBookSelector = PlayerPrefs.GetInt("ToresBookSelector");
 
         for (int i = 0; i < levelButtons.Length; i++)
         {
-            if (i + 1 > MooltosBookSelector)
+            if (i + 1 > ToresBookSelector)
             {
-                levelButtons[i].interactable = false;
                 levelTexts[i].text = "?";
             }
             else
@@ -35,8 +38,54 @@ public class ToresLevelSelector : MonoBehaviour
         }
     }
 
-    public void Select (string levelName)
+    public void Select(string levelName)
     {
         fader.FadeTo(levelName);
+    }
+
+    public void ClickTores(string towerText)
+    {
+        int ToresBookSelector = PlayerPrefs.GetInt("ToresBookSelector");
+
+        if (towerText == "Suklay" && ToresBookSelector < 2)
+        {
+            unlockWhen[0].SetActive(true);
+        }
+        else if (towerText == "Suklay" && ToresBookSelector >= 2)
+        {
+            toresSection.SetActive(false);
+            unlockedToresSection.SetActive(true);
+            unlockedTores[0].SetActive(true);
+        }
+        else if (towerText == "Pagong" && ToresBookSelector < 3)
+        {
+            unlockWhen[1].SetActive(true);
+        }
+        else if (towerText == "Pagong" && ToresBookSelector >= 3)
+        {
+            toresSection.SetActive(false);
+            unlockedToresSection.SetActive(true);
+            unlockedTores[1].SetActive(true);
+        }
+        else if (towerText == "Kandila" && ToresBookSelector < 4)
+        {
+            unlockWhen[2].SetActive(true);
+        }
+        else if (towerText == "Kandila" && ToresBookSelector >= 4)
+        {
+            toresSection.SetActive(false);
+            unlockedToresSection.SetActive(true);
+            unlockedTores[2].SetActive(true);
+        }
+        else if (towerText == "Durian" && ToresBookSelector < 5)
+        {
+            unlockWhen[3].SetActive(true);
+        }
+        else if (towerText == "Durian" && ToresBookSelector >= 5)
+        {
+            toresSection.SetActive(false);
+            unlockedToresSection.SetActive(true);
+            unlockedTores[3].SetActive(true);
+        }
     }
 }
